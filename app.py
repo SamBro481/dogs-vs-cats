f5898b3bcdd82dba6ed83660bd781a7c9df2ed75
Sameer Rehman <kingwarrior481@gmail.com>
2025-08-14 20:01:25 +0530

    Update Gradio code

--- app.py
+++ app.py
@@ -1,7 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#| default_exp app
+
+
+# ## Dogs vs Cats
+
+# In[2]:
+
+
+#| export
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from fastai.vision.all import*
+import gradio as gr
+def is_cat(x): return x[0].isupper()
+
+
+# In[3]:
+
+
+#| export
+
+im = PILImage.create('dog.jpeg')
+im.thumbnail((192,192))
+im
+
+
+# In[4]:
+
+
+#| export
+learn = load_learner('model.pkl')
+
+
+# In[5]:
+
+
+#| export
+learn.predict(im)
+
+# In[6]:
+
+
+#| export
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+learn = load_learner('model.pkl')
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+def classify_image(img):
+    pred, _, probs = learn.predict(img)
+    original_classes = learn.dls.vocab 
+
+    class_map = {False: "Dog", True: "Cat"}
+
+    probs_dict = {class_map[c]: float(probs[i]) for i, c in enumerate(original_classes)}
+
+    return probs_dict  
+
+image_input = gr.Image(type="pil")
+label_output = gr.Label(num_top_classes=None)  # shows all classes as bars
+
+examples = ['dog.jpeg', 'cat.jpg']
+
+
+intf = gr.Interface(
+    fn=classify_image,
+    inputs=image_input,
+    outputs=label_output,
+    examples=examples
+)
+
+intf.launch(inline=False)
+
+
+
+# In[7]:
+
+
+#| export
+m = learn.model
+
+
+
+
+
+
